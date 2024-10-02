8d7b58265831d43ef2bffcad93d881123d6754c6
cla1994 <116500326+cla1994@users.noreply.github.com>
2024-10-02 15:44:34 +0200

    Add files via upload

---
+++ TeaStoreApp/Models/BookmarkedProduct.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaStoreApp.Models
+{
+    public class BookmarkedProduct
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }  // chiave primaria, autoincrementata
+
+        public int ProductId { get; set; }  // prodotto tra favorites
+
+        public string Name {  get; set; }
+
+        public string Detail {  get; set; }
+
+        public int Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool IsBookmarked { get; set; }
+
+
+    }
+}
---
+++ TeaStoreApp/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaStoreApp.Models
+{
+    public class Category
+    {
+        // json property cambia nome di var quando viene serializzata (serve nuget, using newtonsoft in alto)
+        // serve perché noi definiamo var con maiuscole ma json le processa con solo minuscole
+        // si può trovare classe c# da api tramite sito json2csharp.com
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        // => expression body function per risparmiare tempo nello scrivere get e return
+        public string FullImageUrl => AppSettings.ApiUrl + ImageUrl;  
+    }
+}
